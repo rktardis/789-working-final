f6537b151c6e17551af5d3e9ffede93a6d7059f7
rktardis <76893630+rktardis@users.noreply.github.com>
2023-04-22 12:32:39 -0400

    First commit

---
+++ Data analysis one.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 21 15:31:11 2023
+
+@author: richard
+"""
+
+import requests
+
+import pandas as pd
+
+
+# =============================================================================
+# for_clause="county:*"
+# 
+# in_clause="state:*"
+# 
+# payload={"get":"rd33-n4tx", "for":for_clause, "in":in_clause}
+# 
+# api="https://data.ny.gov/resource/"
+# 
+# response=requests.get(api,payload)
+# 
+# row_list=response.json()
+# 
+# colnames=row_list[0]
+# 
+# datarows=row_list[1:]
+# 
+# tolls=pd.DataFrame(columns=colnames, data=datarows)
+# 
+# =============================================================================
+
+#%%
+tolls=pd.read_csv("NYS_Thruway_Origin_and_Destination_Points_for_All_Vehicles_-_15_Minute_Intervals__2020_Q3.csv")
+
+colname={"Payment Type (Cash or E-ZPass)":"Payment","Entrance":"Ent","Interval Beginning Time":"Time","Vehicle Class":"Class","Vehicle Count":"Count"}
+
+tolls=tolls.rename(columns=colname)
+
+syr_exits=["34A","35","36","37","38","39"]
+
+#%%
+# =============================================================================
+# samp=tolls.query("Ent==syr_exits or Exit==syr_exits")
+# =============================================================================
+
+# =============================================================================
+# print(len(tolls))
+# 
+# print( "\nColumns:", list(tolls.columns) )
+# =============================================================================
+
+# =============================================================================
+# pass_class=["2L","2H"]
+# =============================================================================
+
+#%%
+passveh=tolls.query("Class=='2H' or Class=='2L'") 
+
+passpymnts=passveh["Payment"].value_counts()
+
+rev=passveh["Exit"]<passveh["Ent"]
+
+# =============================================================================
+# 
+# 
+# low=passveh[["Ent","Exit"]].min()
+# 
+# high=passveh[["Ent","Exit"]].max()
+# 
+# =============================================================================
+passveh["trip"]=passveh["Ent"]+"-"+passveh["Exit"]
+
+passveh["trip"]=passveh["trip"].where(rev==False,passveh["Exit"]+"-"+passveh["Ent"])
+
+passtrips=passveh.groupby("trip").sum()
+
+passtrips=passtrips.drop(columns="Time")
+
+passtrips=passtrips.sort_values("Count")
+
+passtrips["pct"]=100*passtrips["Count"]/passtrips["Count"].sum()
+
+# =============================================================================
+# pass_veh=tolls.query("Class"==pass_class)
+# =============================================================================
+#%%
+
+comveh=tolls.query("Class=='3L' or Class=='4L'or Class=='3H'or Class=='4H'or Class=='5H'or Class=='6H'or Class=='7H'") 
+
+compymnts=comveh["Payment"].value_counts()
+
+rev=comveh["Exit"]<comveh["Ent"]
+
+# =============================================================================
+# 
+# 
+# low=comveh[["Ent","Exit"]].min()
+# 
+# high=comveh[["Ent","Exit"]].max()
+# 
+# =============================================================================
+comveh["trip"]=comveh["Ent"]+"-"+comveh["Exit"]
+
+comveh["trip"]=comveh["trip"].where(rev==False,comveh["Exit"]+"-"+comveh["Ent"])
+
+comtrips=comveh.groupby("trip").sum()
+
+comtrips=comtrips.drop(columns="Time")
+
+comtrips=comtrips.sort_values("Count")
+
+comtrips["pct"]=100*comtrips["Count"]/comtrips["Count"].sum()
+
+print(comtrips["Count"].sum())
